1104b3976d15dbbea860e756f913a5b87a4a343a
sianturiwilly <prajudiwilliam10@gmail.com>
2023-03-11 20:51:53 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend.")
+}
