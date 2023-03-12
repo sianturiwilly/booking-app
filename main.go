06a11018354dabccd11d59eab2590b16030387d5
sianturiwilly <prajudiwilliam10@gmail.com>
2023-03-12 23:16:11 +0700

    Commit ketiga

--- main.go
+++ main.go
@@ -5,7 +5,10 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "Go Conference"
@@ -41,6 +44,16 @@ func main() {
 		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
 		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-		fmt.Printf("These are all our bookings: %v\n", bookings)
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, firstName)
+		}
+		fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+		if remainingTickets == 0 {
+			// End program
+			fmt.Println("Our conference is booked out. Come back next year.")
+		}
 	}
 }
